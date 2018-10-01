be955dae4c2ace90a065f2497b34b15bad25c570
Maximilien Garenne <maximilien@whatson-web.com>
2018-10-01 15:59:08 +0200

    [integration] add getTypes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import reducerBuilder from './reducerBuilder.js';
 import actionBuilder from './actionBuilder.js';
 import getStore from './storeBuilder.js';
+import getTypes from './typesBuilder';
 
-export { reducerBuilder, actionBuilder, getStore };
+export { reducerBuilder, actionBuilder, getStore, getTypes };
