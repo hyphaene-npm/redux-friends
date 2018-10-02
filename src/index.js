6e8ceb704c16bfc60977badb2a7fb3cb9378fcd5
Maximilien Garenne <maximilien@whatson-web.com>
2018-10-02 12:03:31 +0200

    [integration] clean and remove other helpers to different package, go to v1.0.0

--- src/index.js
+++ src/index.js
@@ -3,24 +3,4 @@ import actionBuilder from './actionBuilder.js';
 import getStore from './storeBuilder.js';
 import getTypes from './typesBuilder';
 
-import {
-	withStateActiveTab,
-	styleTabHandler,
-	toggleHandler,
-	withToggle,
-	withStateOpen,
-	withData,
-} from './recomposeHelpers.js';
-
-export {
-	reducerBuilder,
-	actionBuilder,
-	getStore,
-	getTypes,
-	withStateActiveTab,
-	styleTabHandler,
-	toggleHandler,
-	withToggle,
-	withStateOpen,
-	withData,
-};
+export { reducerBuilder, actionBuilder, getStore, getTypes };
