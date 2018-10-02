a97aa93c08b3275c1369f165e1b108863aa8c4ae
Maximilien Garenne <maximilien@whatson-web.com>
2018-10-02 12:04:10 +0200

    [integration] dist

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
-exports.withData = exports.withStateOpen = exports.withToggle = exports.toggleHandler = exports.styleTabHandler = exports.withStateActiveTab = exports.getTypes = exports.getStore = exports.actionBuilder = exports.reducerBuilder = undefined;
+exports.getTypes = exports.getStore = exports.actionBuilder = exports.reducerBuilder = undefined;
 
 var _reducerBuilder = require('./reducerBuilder.js');
 
@@ -21,17 +21,9 @@ var _typesBuilder = require('./typesBuilder');
 
 var _typesBuilder2 = _interopRequireDefault(_typesBuilder);
 
-var _recomposeHelpers = require('./recomposeHelpers.js');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.reducerBuilder = _reducerBuilder2.default;
 exports.actionBuilder = _actionBuilder2.default;
 exports.getStore = _storeBuilder2.default;
 exports.getTypes = _typesBuilder2.default;
-exports.withStateActiveTab = _recomposeHelpers.withStateActiveTab;
-exports.styleTabHandler = _recomposeHelpers.styleTabHandler;
-exports.toggleHandler = _recomposeHelpers.toggleHandler;
-exports.withToggle = _recomposeHelpers.withToggle;
-exports.withStateOpen = _recomposeHelpers.withStateOpen;
-exports.withData = _recomposeHelpers.withData;
