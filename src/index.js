6beca740cd4a381909b7114dd246b907e6a75e41
Maximilien Garenne <maximilien@whatson-web.com>
2018-10-05 11:25:26 +0200

    [integration] add constants

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import createReducer from './createReducer.js';
 import createAction from './createAction.js';
 import createStore from './createStore.js';
 import createTypes from './createTypes.js';
+import { PAGES, DATA } from './constants';
 
-export { createReducer, createAction, createStore, createTypes };
+export { createReducer, createAction, createStore, createTypes, PAGES, DATA };
