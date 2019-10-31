b38025344add799d89249af20c33eeddfe7f91f4
Maximilien Garenne <maximilien.garenne@gmail.com>
2019-10-31 13:10:35 +0100

    [integration] update rollup config related to external deps

--- rollup/constants.js
+++ rollup/constants.js
@@ -4,47 +4,22 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import localResolve from 'rollup-plugin-local-resolve';
-import postcss from 'rollup-plugin-postcss';
-import autoprefixer from 'autoprefixer';
+// import { terser } from 'rollup-plugin-terser';
+// import typescript from 'rollup-plugin-typescript2';
 
 export const plugins = [
 	peerDepsExternal(),
-	postcss({ extract: true, plugins: [autoprefixer] }),
+	babel({ exclude: 'node_modules/**' }),
+
 	babel({ exclude: 'node_modules/**' }),
 	localResolve(),
 	resolve(),
 	commonjs(),
 	filesize(),
 ];
-const react = 'react';
-const PropTypes = 'prop-types';
-const reactDom = 'react-dom';
-const thunk = 'redux-thunk';
-const reduxActions = 'redux-actions';
-const reduxTypes = 'redux-types';
-const reduxPersist = 'redux-persist';
 const redux = 'redux';
-const reduxDevtoolsExtension = 'redux-devtools-extension';
-const createHistory = 'history/createHashHistory';
-const reactRouterRedux = 'react-router-redux';
-const autoMergeLevel2 = 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-const storage = 'redux-persist/lib/storage';
 
-export const external = [
-	react,
-	reactDom,
-	PropTypes,
-	thunk,
-	reduxPersist,
-	reduxActions,
-	redux,
-	reduxDevtoolsExtension,
-	createHistory,
-	reactRouterRedux,
-	autoMergeLevel2,
-	storage,
-	reduxTypes,
-];
+export const external = [redux];
 
 const createObjectWithKeyEqualValues = list =>
 	list.reduce((acc, current) => {
